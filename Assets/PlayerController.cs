327f75c1edae23a7eb2d7b4d4fb3d2f8a07d23fa
Krishna Tholudur <krishna.tholudur@gmail.com>
2026-01-22 13:25:54 -0800

    end of week2 commit - added physics based platformer movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,17 +6,25 @@ public class PlayerController : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 5.0f;
+    Rigidbody2D rb;
+    bool isGrounded;
+    int score = 0;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementDistanceX = movementX * speed * Time.deltaTime;
-        float movementDistanceY = movementY * speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        //float movementDistanceX = movementX * speed * Time.deltaTime;
+        //float movementDistanceY = movementY * speed * Time.deltaTime;
+        //transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
+        if (isGrounded && movementY > 0)
+        {
+            rb.AddForce(new Vector2(0, 100));
+        }
     }
     void OnMove(InputValue value)
     {
@@ -25,5 +33,31 @@ public class PlayerController : MonoBehaviour
         movementY = v.y;
         Debug.Log("Movement X = " + movementX);
         Debug.Log("Movement Y = " + movementY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            score++;
+            collision.gameObject.SetActive(false);
+            Debug.Log("Score: " + score);
+        }
     }
 }
